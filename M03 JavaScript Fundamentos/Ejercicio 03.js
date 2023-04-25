bb291c94ed24cdd2047add4d24cb0c1651bd7a55
compudefierro <jorgeeldri@gmail.com>
2023-04-25 12:57:28 -0300

    Finalizacion ejercicios M03

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -58,14 +58,20 @@ function esPar(num) {
    }
    else {
       return false;
-   }
-}
+   };
+};
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   if ((num % 2) != 0) {
+      return true;
+   }
+   else {
+      return false;
+   }
+};
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
